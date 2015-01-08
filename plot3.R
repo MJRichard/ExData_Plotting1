374829038280f0e4f23816ba8f9acf28b29fc5ae
Martin Richard <Mr.Martin.J.Richard@gmail.com>
2015-01-08 01:33:58 -0500

    Added note to R files about where data is from

--- plot3.R
+++ plot3.R
@@ -1,3 +1,7 @@
+#Assume data is in working directory
+#Data is from UC Irving Machine Learning Repository
+#Data can be downloaded at https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+
 housedata<-read.table('household_power_consumption.txt', header=TRUE, sep=';', na.strings='?')
 housedata2<-subset(housedata, (Date=='1/2/2007' | Date=='2/2/2007'))
 
